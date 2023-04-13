7afbb1850a7243ce42367a0be5abc927910ed8eb
Viharika <viharika807@gmail.com>
2023-04-13 23:18:13 +0530

    "updated and final"

--- client/src/App.js
+++ client/src/App.js
@@ -1,116 +1,143 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-const API_BASE = 'http://localhost:3002';
+const API_BASE = "http://localhost:3002";
 
 function App() {
-    const [todos, setTodos] = useState([]);
-    const [popupActive, setPopupActive] = useState(false);
-    const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
 
-    useEffect(() => {
-            GetTodos();
-    }, []);
+  useEffect(() => {
+    GetTodos();
+  }, []);
 
-    const GetTodos = () => {  
-        fetch(API_BASE + "/todo")
-            .then(res => res.json())
-            .then(data => setTodos(data))
-            .catch(err => console.log("Error: ", err));
-    }   
+  const GetTodos = async () => {
+    try {
+      const res = await fetch(API_BASE + "/todo");
+      const data = await res.json();
+      setTodos(data);
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
 
-    const completeTodo = async id => {
-        const data = await fetch(API_BASE + "/todo/complete/" + id, {
-            method: "PUT",
+  const completeTodo = async (id) => {
+    try {
+      const response = await fetch(API_BASE + "/todo/complete/" + id, {
+        method: "PUT",
+      });
+      const data = await response.json();
+
+      setTodos((todos) =>
+        todos.map((todo) => {
+          if (todo._id === data._id) {
+            todo.complete = data.complete;
+          }
+
+          return todo;
         })
-            .then(res => res.json())   
-
-        setTodos(todos => todos.map(todo => {
-            if (todo._id === data._id) {
-                todo.complete = data.complete;
-            }
-
-            return todo;
-        }));
+      );
+    } catch (error) {
+      console.error("Error completing todo:", error);
     }
+  };
 
-    const deleteTodo = async (id) => {
-        const response = await fetch(`http://localhost:3002/todo/delete/${id}`, {
-          method: "DELETE",
-        });
-        
-        if (response.ok) {
-          setTodos(todos.filter((todo) => todo._id !== id));
-        } else {
-          console.error("Failed to delete todo.");
-        }
-      };
+  const deleteTodo = async (id) => {
+    try {
+      const response = await fetch(`http://localhost:3002/todo/delete/${id}`, {
+        method: "DELETE",
+      });
 
-    const addTodo = async () => {
-        const data = await fetch(API_BASE + "/todo/new", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                text: newTodo
-            })
-        }).then(res => res.json());
-
-        setTodos(todos => [...todos, data]);
-        setPopupActive(false);
-        setNewTodo("");
+      if (response.ok) {
+        setTodos(todos.filter((todo) => todo._id !== id));
+      } else {
+        throw new Error("Failed to delete todo.");
+      }
+    } catch (error) {
+      console.error(error);
+      // Display error message to user
+      alert("An error occurred while deleting the todo item.");
     }
+  };
 
+  const addTodo = async () => {
+    try {
+      const response = await fetch(API_BASE + "/todo/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          text: newTodo,
+        }),
+      });
 
+      const data = await response.json();
 
+      setTodos((todos) => [...todos, data]);
+      setPopupActive(false);
+      setNewTodo("");
+    } catch (err) {
+      console.log("Error: ", err);
+    }
+  };
 
-	return (
-		<div className="App">
-			<h1>Howdy!</h1>
-            <h4>Your tasks for the day:</h4>
+  return (
+    <div className="App">
+      <h1>Howdy!</h1>
+      <h4>Your tasks for the day:</h4>
 
-            <div className="todos">
-                {todos.map(todo => (
+      <div className="todos">
+        {todos.map((todo) => (
+          <div
+            className={"todo " + (todo.complete ? "is-complete" : "")}
+            key={todo.id}
+            onClick={() => completeTodo(todo._id)}
+          >
+            <div className="checkbox"></div>
 
-                <div className={"todo " + (todo.complete ? "is-complete" : "")} 
-                     key={todo.id} onClick={() => completeTodo(todo._id)}>
+            <div className="text">{todo.text}</div>
 
-                    <div className="checkbox"></div>
-
-                    <div className="text">{ todo.text }</div>
-
-                    <div className="delete-todo" onClick={() => deleteTodo
-                    (todo._id)}>x</div>
-                </div>
-                ))}
-
-                {
-                    todos.length === 0 ? (
-                        <div className="no-todos">No tasks yet!</div>
-                    ) : ''
-                }
+            <div className="delete-todo" onClick={() => deleteTodo(todo._id)}>
+              x
             </div>
+          </div>
+        ))}
 
+        {todos.length === 0 ? (
+          <div className="no-todos">No tasks yet!</div>
+        ) : (
+          ""
+        )}
+      </div>
 
-            <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
-		
-            {popupActive ? (
-                <div className="popup">
-                    <div className="closePopup" onClick={() => setPopupActive
-                    (false)}>x</div>
-                        <div className="content">
-                            <h3>Add Task</h3>
-                            <input type="text" 
-                            className='add-todo-input'
-                            onChange={e => setNewTodo(e.target.value)} 
-                            value={newTodo}/>
-                        <div className="button" onClick={addTodo}>Create Task</div>
+      <div className="addPopup" onClick={() => setPopupActive(true)}>
+        +
+      </div>
 
-                        </div>
-                </div>
-            ) : ''}     
+      {popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>
+            x
+          </div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input
+              type="text"
+              className="add-todo-input"
+              onChange={(e) => setNewTodo(e.target.value)}
+              value={newTodo}
+            />
+            <div className="button" onClick={addTodo}>
+              Create Task
+            </div>
+          </div>
         </div>
-	);
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,22 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 // Use bodyParser to parse JSON request bodies
 app.use(bodyParser.json());
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
-// Connect to MongoDB database using mongoose 
-mongoose.connect(('mongodb+srv://viharikaa:mini%40123@mydata.8zx6rvr.mongodb.net/todoAppData'),{
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+// Connect to MongoDB database using mongoose
+mongoose.connect(
+  "mongodb+srv://viharikaa:mini%40123@mydata.8zx6rvr.mongodb.net/todoAppData",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 // import the schema from './models/Todo.js'
-const Todo = require('./models/Todo');
+const Todo = require("./models/Todo");
 /*
 the below API endpoint should return all the todos in the todos.json file
 Sample response:
@@ -34,17 +37,16 @@ Sample response:
     ]
 */
 
-app.get('/todo', async (req, res) => {
-    Todo.find({}, (err, data) => {
-        if (err) {
-          console.error(err);
-          res.status(500).send('An error occurred while retrieving the data.');
-        } else {
-          res.send(data);
-        }
-    });
+app.get("/todo", async (req, res) => {
+  Todo.find({}, (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("An error occurred while retrieving the data.");
+    } else {
+      res.send(data);
+    }
+  });
 });
-
 
 // the below API endpoint should add a new todo to the todos.json file
 /*
@@ -63,20 +65,19 @@ Sample response:
 */
 
 // Define a route to add a new todo
-app.post('/todo/new', async (req, res) => {
-    try {
-        const newTodo = new Todo({
-            text: req.body.text,
-            complete: false
-        });
-        const savedTodo = await newTodo.save();
-        res.json(savedTodo);
-    }catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error adding todo' });
-    }
+app.post("/todo/new", async (req, res) => {
+  try {
+    const newTodo = new Todo({
+      text: req.body.text,
+      complete: false,
+    });
+    const savedTodo = await newTodo.save();
+    res.json(savedTodo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error adding todo" });
+  }
 });
-
 
 // the below API endpoint should delete a todo from the todos.json file
 /*
@@ -90,18 +91,16 @@ Sample response:
 }
 */
 
-
-app.delete('/todo/delete/:id', async (req, res) => {
-    const id = req.params.id;
-    console.log("Deleting todo with ID:", id);
-    try {
-      const deletedTodo = await Todo.deleteOne({_id: id});
-      res.json({id: deletedTodo._id});
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  });
-  
+app.delete("/todo/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log("Deleting todo with ID:", id);
+  try {
+    const deletedTodo = await Todo.findOneAndDelete({ _id: id });
+    res.json({ id: deletedTodo._id });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // the below API endpoint should toggle the complete status of a todo in the todos.json file
 /*
@@ -115,21 +114,16 @@ Sample response:
 }
 */
 
-
-app.put('/todo/complete/:id', async (req, res) => {
-    try {
-      const todo = await Todo.findById(req.params.id);
-      todo.complete = !todo.complete;
-      const updatedTodo = await todo.save();
-      res.json(updatedTodo);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('An error occurred while completing the todo.');
-    }
-  });
-  
-
-
+app.put("/todo/complete/:id", async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    const updatedTodo = await todo.save();
+    res.json(updatedTodo);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("An error occurred while completing the todo.");
+  }
+});
 
 app.listen(3002, () => console.log("Server is running on port 3002"));
-
